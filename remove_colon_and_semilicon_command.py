b616eca3a279c3ea2283142d8a6de30bbea8e07c
campykid <vladkhvo@gmail.com>
2015-11-24 16:39:43 +0300

    DRY

--- remove_colon_and_semilicon_command.py
+++ remove_colon_and_semilicon_command.py
@@ -3,8 +3,10 @@ import sublime, sublime_plugin
 class RemoveColonAndSemiliconCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		REPL_STRING = ""
+		REPL_PATTERN = [':', ';']
 		for region in self.view.sel():
 			content = self.view.substr(region)
-			content = content.replace(':', REPL_STRING)
-			content = content.replace(';', REPL_STRING)
-			self.view.replace(edit, region, content)
+
+			for pattern in REPL_PATTERN:
+				content = content.replace(pattern, REPL_STRING)
+				self.view.replace(edit, region, content)
